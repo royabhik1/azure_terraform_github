efa334cae2c803f7d37d7fd2caf52eccfbfce841
royabhik1 <roy.abhik@gmail.com>
2022-04-15 14:27:17 -0500

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -7,3 +7,20 @@ variable "resource_group_location" {
   default = "eastus"
   description   = "Location of the resource group."
 }
+
+variable "subscription_id" {
+    description = "The subscription ID to be used to connect to Azure"
+    type = string
+}
+variable "client_id" {
+    description = "The client ID to be used to connect to Azure"
+    type = string
+}
+variable "client_secret" {
+    description = "The client secret to be used to connect to Azure"
+    type = string
+}
+variable "tenant_id" {
+    description = "The tenant ID to be used to connect to Azure"
+    type = string
+}
